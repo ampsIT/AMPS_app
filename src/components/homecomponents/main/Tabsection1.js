82a7bceb64a68a1434e666203ad302679fa938c8
u2015s <utkarshsingh2015@gmail.com>
2020-08-04 13:57:19 +0530

    Card Ui updated

--- src/components/homecomponents/main/Tabsection1.js
+++ src/components/homecomponents/main/Tabsection1.js
@@ -12,21 +12,42 @@ import { View, Text, ActivityIndicator, StyleSheet, Modal, Platform, Linking, Te
 
     const Item = ({postTitle,content,image,timestamp}) => (
             <View style={styles.item}>
-              <Image
-              resizeMode='cover'
-              style={styles.CardImage}
-              source={{uri: image}}
-              />
-              <Text style={styles.title}>
-                {postTitle}
-              </Text>
-              <Text style={styles.timestamp}>
-                {timestamp}
-              </Text>
-              <Text style={styles.paragraph}>
-              {content}
-              
-              </Text>
+              <View
+               style={styles.IView}
+              >
+                <Image
+                resizeMode='cover'
+                style={styles.CardImage}
+                source={{uri: image}}
+                // source={{uri: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg'}}
+                />
+                </View>
+
+              <View
+              style={styles.TView}
+              >
+                <Text style={styles.timestamp}>
+                  {timestamp}
+                </Text>
+              </View>
+
+              <View
+              style={styles.HView}
+              >
+                <Text style={styles.title}>
+                  {postTitle}
+                  {/* THIS IS THE LONG HEADING */}
+                </Text>
+              </View>
+
+              <View
+              style={styles.CView}
+              >
+                <Text style={styles.paragraph}>
+                  {/* {content} */}
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has
+                </Text>
+              </View>
             </View>
           );
      
@@ -85,30 +106,55 @@ import { View, Text, ActivityIndicator, StyleSheet, Modal, Platform, Linking, Te
     const styles = StyleSheet.create({
         container: {
           flex: 1,
-          marginTop: StatusBar.currentHeight || 0,
+          // marginTop: StatusBar.currentHeight || 0,
+          // marginTop: HW'',
+          marginTop:hp2dp('0.5%')
           // alignItems: 'center'
         },
         item: {
           backgroundColor: 'white',
-          padding: wp2dp('5%'),
-          marginVertical: hp2dp('1%'),
-          marginHorizontal: wp2dp('5%'),
-          width:wp2dp('90%')
+          padding: wp2dp('4%'),
+          marginVertical: hp2dp('2%'),
+          marginHorizontal: wp2dp('3%'),
+          width:wp2dp('95%'),
+          elevation:3,
+          borderRadius:8
         },
         title: {
-          fontSize: 32,
+          fontSize: 30,
         },
         CardImage: {
-          width: wp2dp('80%'),
+          width: wp2dp('95%'),
           height:hp2dp('40%'),
-          marginBottom:hp2dp('1%')
+          // marginBottom:hp2dp('1%'),
+          marginTop:hp2dp('-2.3%'),
+          // backgroundColor:'black',
+          borderTopRightRadius:8,
+          borderTopLeftRadius:8
         },
         paragraph: {
-          fontSize: 14
+          fontSize: 16
         },
         timestamp:{
-          fontSize:11,
-          marginBottom:hp2dp('2%')
+          fontSize:14,
+          marginVertical:hp2dp('1%'),
+          color:'grey'
+        },
+        IView:{
+          alignItems: 'center',
+        },
+        HView:{
+          // marginTop:hp2dp('1%'),
+          // backgroundColor:'red',
+          alignItems:'flex-start'
+        },
+        TView: {
+          // backgroundColor:'red',
+
+
+        },
+        CView:{
+          // backgroundColor:'red',
 
         }
         
